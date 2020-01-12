6b728c52cdbf6c16f58353fa41f678af2cc8d515
Wessel <wesselvanz@gmail.com>
2020-01-12 14:13:46 +0100

    changed the map layout

--- app/src/main/java/com/wessel/PilsFinder/controller/activity/MapActivity.java
+++ app/src/main/java/com/wessel/PilsFinder/controller/activity/MapActivity.java
@@ -1,6 +1,7 @@
 package com.wessel.PilsFinder.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
@@ -8,7 +9,9 @@ import com.wessel.PilsFinder.R;
 import com.wessel.PilsFinder.model.Beer.BeerDB;
 import com.wessel.PilsFinder.model.Pub.PubDB;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends FragmentActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
